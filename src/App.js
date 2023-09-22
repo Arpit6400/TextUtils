cc17a48b0242a9ca51ae5be430ad7030304df9d9
Arpit Hamjade <arpitshirishhamjade@gmail.com>
2023-09-22 13:27:25 +0530

    temp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+let name="Arpit";
 function App() {
   return (
-    <div className="blank">Nice</div>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <div className="container">
+      <h1>Hello {name}</h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quos sit quod numquam repudiandae! Dolorum deleniti ratione nisi sunt, accusantium vitae sapiente natus ipsum in. Dicta cumque sed ratione? Expedita quam et eos facere.
+    </div>
+    </>
   );
 }
 
