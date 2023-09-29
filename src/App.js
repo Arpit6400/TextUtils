08481d45ea4b687d1cd274d0b92f6efa88762f04
Arpit Hamjade <arpitshirishhamjade@gmail.com>
2023-09-29 15:43:35 +0530

    Update dependencies, add react-router-dom, react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
 import Alert from './components/Alert';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import React,{useState} from 'react'
+import React,{useState} from 'react';
+import {BrowserRouter as Router, Switch,Routes ,Route, Link} from "react-router-dom"
 function App() {
   const [mode, setMode] = useState('light');
   const toggleMode= ()=>{
@@ -29,13 +30,23 @@ function App() {
   }
   return (
     <>
-  
+    <Router>
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
     <Alert alert={alert}/>
     <div className="container my-3">
-    <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
-    {/* <About/> */}
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />} />
+          </Routes>
     </div>
+    </Router>
+    {/* <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
+    <div className="container my-3">
+      <Switch></Switch>
+    <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
+    <About/>
+    </div> */}
     </>
   );
 }
